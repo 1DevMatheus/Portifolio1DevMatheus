072110bf9a71d7fb4bc3f58bc53bcee6de9c0224
Matheus S. Oliveira <matheuskiller6@gmail.com>
2023-04-02 18:26:10 -0300

    adicionando a calculadora

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ var img = document.getElementById("myImg");
 var img2 = document.getElementById("myImg2");
 var img3 = document.getElementById("myImg3");
 var img4 = document.getElementById("myImg4");
+var img6 = document.getElementById("myImg6");
 
 var modalImg = document.getElementById("img01");
 var modalImg2 = document.getElementById("img02");
 var modalImg3 = document.getElementById("img03");
 var modalImg4 = document.getElementById("img03");
+var modalImg6 = document.getElementById("img06");
 
 
 var captionText = document.getElementById("caption");
@@ -33,6 +35,12 @@ img3.onclick = function(){
 }
 
 img4.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+img6.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
